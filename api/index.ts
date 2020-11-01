e50349f181d3882ccf17a9f091c06e5847446a07
Richard G <rwguo@andrew.cmu.edu>
2020-10-31 23:45:58 -0500

    made some modifications

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,51 @@
-import * as http from "http";
-import App from "./app";
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as morgan from "morgan";
+import * as cors from "cors";
+import * as mongoose from "mongoose";
+import UserRouter from "./routes/users";
 
-const port = 3080;
+const port = process.env.SERVER_PORT || 3080;
+const database =
+  process.env.DATABASE ||
+  process.env.MONGODB_URI ||
+  "mongodb://localhost:27017";
 
-App.set("port", port);
-const server = http.createServer(App);
-server.listen(port);
+// https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-react-app-with-nodejs-backend-typescript-version-27a6a283a7c5
+// https://github.com/bbachi/react-nodejs-typescript-example
+// https://github.com/seanpmaxwell/express-generator-typescript
 
-server.on("listening", function(): void {
-    const addr = server.address();
-    const bind = (typeof addr === "string") ? `pipe ${addr}` : `port ${addr.port}`;
-    console.log(`Listening on ${bind}`);
- });
+const app: express.Application = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(process.cwd() + "/client/build/"));
 
-module.exports = App;
+// Set up CORS policy
+app.use(cors());
+
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch(() => console.log("Failed to connect to DB!"));
+
+// Morgan is for logging
+app.use(morgan("dev"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(process.cwd() + "/client/build/index.html");
+});
+
+// api route
+app.use("/api/users", UserRouter);
+
+// handle undefined routes
+app.use("*", (req, res, next) => {
+  res.send("Make sure url is correct!!!");
+});
+
+
+app.listen(port);
+console.log("App listening on port " + port);

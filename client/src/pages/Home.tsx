36f477a7e3dd3485ae2dd0d71ac6ea47d48d0d81
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-14 16:04:58 -0500

    updated home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom"; 
+import { Grid, Button } from "semantic-ui-react";
 import "./Home.css";
+import 'semantic-ui-css/semantic.min.css'
 
 function Home() {
+  document.title = "CMU Lost and Found"
   return (
     <Grid>
-      <div className="title">This is the Lost and Found Home Page</div>
-      <Link to="/page2">Hi</Link>
+      <main>
+        <h1 className="title">Welcome to the<br></br>CMU Lost and Found</h1>
+        <img src="/dog-logo.png" alt="CMU Lost and Found Logo" width="300"></img>
+        <br></br>
+        <br></br>
+        <Link to = "/Admin"><Button>Admin Login</Button></Link>
+      </main>
     </Grid>
   );
 };

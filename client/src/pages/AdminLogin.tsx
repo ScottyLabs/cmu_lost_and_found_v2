5f1f7503a14395ce08edb2739247a5fe411f6fc7
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:38 -0500

    Create AdminLogin.tsx

---
+++ client/src/pages/AdminLogin.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import LoginForm from "../components/Form";
+import "./AdminLogin.css";
+import "semantic-ui-css/semantic.min.css"
+
+function AdminLogin() {
+  document.title = "CMU Lost and Found"
+  return (
+    <Grid>
+      <main id="form">
+        <h1 id="title">Admin Login</h1>
+        <LoginForm></LoginForm>
+      </main>
+    </Grid>
+  );
+};
+
+export default AdminLogin;

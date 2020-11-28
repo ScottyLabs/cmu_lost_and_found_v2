08bf30c95ce0cf117d0b598c264c9fce24aee2b2
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:35 -0500

    Update Admin.tsx

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,17 +1,23 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom"; 
-import { Grid, Button } from "semantic-ui-react";
-import "./Home.css";
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./Admin.css";
+import VerticalMenu from "../components/VerticalMenu"
+import "semantic-ui-css/semantic.min.css"
 
 function Admin() {
   document.title = "CMU Lost and Found"
   return (
     <Grid>
-      <main>
-        <h1 className="title">Admin Page</h1>
-        <Link to = "/"><Button>Home</Button></Link>
-      </main>
+      <Grid.Row>
+        <Grid.Column width={3} id="side">
+          <VerticalMenu></VerticalMenu>
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <main>
+            <h1 id="title">Admin Page</h1>
+          </main>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   );
 };

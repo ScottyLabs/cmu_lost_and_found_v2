3df01511f1ffd4763e5c1b185696e1bbb117de18
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:43 -0500

    Create Items.tsx

---
+++ client/src/pages/Items.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./Items.css";
+import "semantic-ui-css/semantic.min.css"
+
+function Items() {
+  document.title = "CMU Lost and Found"
+  return (
+    <Grid>
+      <main>
+        <h1 id="title">Item List</h1>
+      </main>
+    </Grid>
+  );
+};
+
+export default Items;

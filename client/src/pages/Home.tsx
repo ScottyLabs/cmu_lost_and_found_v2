0f24a584fbfb9d9ee5ab788958d1dce19d06adf6
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:41 -0500

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom"; 
 import { Grid, Button } from "semantic-ui-react";
 import "./Home.css";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css"
 
 function Home() {
   document.title = "CMU Lost and Found"
   return (
     <Grid>
       <main>
-        <h1 className="title">Welcome to the<br></br>CMU Lost and Found</h1>
+        <h1 id="title">Welcome to the<br></br>CMU Lost and Found</h1>
         <img src="/dog-logo.png" alt="CMU Lost and Found Logo" width="300"></img>
         <br></br>
         <br></br>
-        <Link to = "/Admin"><Button>Admin Login</Button></Link>
+        <Link to = "/AdminLogin"><Button>Admin Login</Button></Link>
+        <Link to = "/Items"><Button>View Items</Button></Link>
       </main>
     </Grid>
   );

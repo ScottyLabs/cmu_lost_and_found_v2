f6051344605116f6ea477ddd440dce9cf3715f72
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:14 -0500

    Create VerticalMenu.js

---
+++ client/src/components/VerticalMenu.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Dropdown, Menu } from 'semantic-ui-react'
+
+export default class VerticalMenu extends Component {
+  state = { activeItem: 'account' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu secondary vertical>
+        <img src="/dog-logo.png" alt="CMU Lost and Found Logo" width="210"></img>
+        <br></br>
+        <br></br>
+        <Menu.Item
+          name='account'
+          active={activeItem === 'account'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='settings'
+          active={activeItem === 'settings'}
+          onClick={this.handleItemClick}
+        />
+        <Dropdown item text='Display Options'>
+          <Dropdown.Menu>
+            <Dropdown.Header>Text Size</Dropdown.Header>
+            <Dropdown.Item>Small</Dropdown.Item>
+            <Dropdown.Item>Medium</Dropdown.Item>
+            <Dropdown.Item>Large</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
+    )
+  }
+}

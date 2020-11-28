3a399a05328d5026805a9d752ea0b386d3877040
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-11-28 17:55:12 -0500

    Create Form.js

---
+++ client/src/components/Form.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Button, Form} from "semantic-ui-react"
+import { Link } from "react-router-dom"; 
+
+const LoginForm = () => (
+  <Form>
+    <Form.Field>
+      <Form.Input label="Username" placeholder="Username"/>
+    </Form.Field>
+    <Form.Field>
+      <Form.Input label="Password" placeholder="Password" type="password"/>
+    </Form.Field>
+    <Form.Field>
+      <Link to="/reset" id="forgot">Forgot Password?</Link>
+    </Form.Field>
+    <Link to = "/Admin"><Button type="submit">Submit</Button></Link>
+  </Form>
+)
+
+export default LoginForm

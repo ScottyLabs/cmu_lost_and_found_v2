dcdf525aa783cbd9059a630f8fc9aaaa53926884
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-12-26 16:07:09 -0500

    Update Items.tsx

--- client/src/pages/Items.tsx
+++ client/src/pages/Items.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import "./Items.css";
+import VerticalMenu from "../components/VerticalMenu";
+import FilterBar from "../components/FilterBar";
 import "semantic-ui-css/semantic.min.css";
 
 function Items() {
   document.title = "CMU Lost and Found";
   return (
     <Grid>
-      <main>
-        <h1 id="title">Item List</h1>
-      </main>
+      <Grid.Row>
+        <Grid.Column width={3} id="side">
+          <VerticalMenu></VerticalMenu>
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <main>
+            <h1 id="title">Item List</h1>
+            <div id="filter-bar">
+              <FilterBar></FilterBar>  
+            </div>
+          </main>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   );
 };

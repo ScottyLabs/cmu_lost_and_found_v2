c2e825726a9e26ac9da4b1075731ab754f8a201b
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2021-01-03 01:37:20 -0500

    Renamed login form

--- client/src/pages/AdminLogin.tsx
+++ client/src/pages/AdminLogin.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
-import LoginForm from "../components/Form";
+import LoginForm from "../components/LoginForm";
 import "./AdminLogin.css";
 import "semantic-ui-css/semantic.min.css";
 

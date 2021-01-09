f13bdec5586bd889d4d45b606b9fba87f2b558eb
Richard G <rwguo@andrew.cmu.edu>
2021-01-09 16:54:38 -0600

    added authentication code

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ npm install
 npm start
 ```
 
+The API currently runs on port 3080. The client (development only) runs on port 3000. For production, build the client and deploy it with the API on port 3080.
+
 Credits to Bhargav Bachina for template: 
 https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-react-app-with-nodejs-backend-typescript-version-27a6a283a7c5
 https://github.com/bbachi/react-nodejs-typescript-example
--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,20 +1,59 @@
 import { Request, Response, Router } from "express";
 import * as mongoose from "mongoose";
-import User from "../models/User"
+import UserController from "../controllers/UserController";
+import User, { IUser } from "../models/User"
 // const User = require("../models/User")
 const router = Router();
 
-router.get("/all", async (req: Request, res: Response) => {
-  let richard = new User({name: "Richard"});
-  richard.save(function (err, product) {
-    if (err) console.log(err);
-    console.log(product);
-  });
-  User.find(function (err, users) {
-    if (err) return console.error(err);
-    console.log(users);
-  });
-  return res.status(200).json({ bob: "hello" });
+/**
+ * Register a user with a username and password
+ * Fails if password too short or username already exists
+ * body {
+ *  username: username,
+ *  password: password
+ * }
+ */
+router.post("/register", (req: Request, res: Response) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  UserController.createUser(username, password, (err:string, user:IUser) => {
+    if (err != null) {
+      return res.status(401).send(err);
+    }
+    return res.status(200).json({user: user});
+  })
+  
 });
+
+/**
+   * Login a user with a username and password.
+   * Otherwise, 401.
+   *
+   * body {
+   *  username, username
+   *  password: password
+   * }
+   *
+   */
+  router.post('/login',
+    function(req: Request, res: Response, next){
+      let username = req.body.username;
+      let password = req.body.password;
+
+      UserController.loginWithPassword(
+        username,
+        password,
+        function (err, user) {
+          if (err || !user) {
+            return res.status(401).send(err);
+          }
+          return res.json({
+            user: user,
+          });
+        }
+      );
+
+  });
 
 export default router;

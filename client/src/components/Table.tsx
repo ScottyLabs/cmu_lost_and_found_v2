f5f9e08ec15f220eace51c27a195e0995c847667
Richard G <rwguo@andrew.cmu.edu>
2021-01-10 13:17:38 -0600

    added table page

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import "./Table.css";
+
+const TableExample = () => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Date Found</Table.HeaderCell>
+        <Table.HeaderCell>Time Found</Table.HeaderCell>
+        <Table.HeaderCell>Object</Table.HeaderCell>
+        <Table.HeaderCell>Where Found</Table.HeaderCell>
+        <Table.HeaderCell>Description</Table.HeaderCell>
+        <Table.HeaderCell>Category</Table.HeaderCell>
+        <Table.HeaderCell>Where to Retrieve</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Cell1</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+
+    {/* <Table.Footer>
+      <Table.Row>
+        <Table.HeaderCell colSpan="3">
+          <Menu floated="right" pagination>
+            <Menu.Item as="a" icon>
+              <Icon name="chevron left" />
+            </Menu.Item>
+            <Menu.Item as="a">1</Menu.Item>
+            <Menu.Item as="a">2</Menu.Item>
+            <Menu.Item as="a">3</Menu.Item>
+            <Menu.Item as="a">4</Menu.Item>
+            <Menu.Item as="a" icon>
+              <Icon name="chevron right" />
+            </Menu.Item>
+          </Menu>
+        </Table.HeaderCell>
+      </Table.Row>
+    </Table.Footer> */}
+  </Table>
+);
+
+export default TableExample;
---
+++ client/src/pages/TablePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "../components/Table";
+
+function Page2() {
+  return (
+    <>
+      <div className="title">Here is the table</div>
+      <Table></Table>
+    </>
+  );
+}
+
+export default Page2;

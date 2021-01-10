19488438d4924fad1b2980f985fb1badca594566
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2021-01-10 16:20:16 -0500

    Updated add item form

--- client/src/components/AddModal.js
+++ client/src/components/AddModal.js
@@ -17,21 +17,21 @@ function exampleReducer(state, action) {
   }
 }
 
-const locations = [
-  { key: 'baker', text: 'Baker Hall', value: 'baker hall' },
-  { key: 'cohon', text: 'Cohon University Center', value: 'cohon university center' },
-  { key: 'doherty', text: 'Doherty Hall', value: 'doherty hall' },
-  { key: 'gates', text: 'Gates Center', value: 'gates center' },
-  { key: 'hamburg', text: 'Hamburg Hall', value: 'hamburg hall' },
-  { key: 'hunt', text: 'Hunt Library', value: 'hunt library' },
-  { key: 'hamerschlag', text: 'Hamerschlag Hall', value: 'hamerschlag hall' },
-  { key: 'margaret', text: 'Margaret Morrison Hall', value: 'margaret morrison hall' },
-  { key: 'porter', text: 'Porter Hall', value: 'porter hall' },
-  { key: 'posner', text: 'Posner Hall', value: 'posner hall' },
-  { key: 'sorrells', text: 'Sorrells Library', value: 'sorrells library' },
-  { key: 'tepper', text: 'Tepper Building', value: 'tepper building' },
-  { key: 'wean', text: 'Wean Hall', value: 'wean hall' },
-]
+// const locations = [
+//   { key: 'baker', text: 'Baker Hall', value: '' },
+//   { key: 'cohon', text: 'Cohon University Center', value: 'cohon university center' },
+//   { key: 'doherty', text: 'Doherty Hall', value: 'doherty hall' },
+//   { key: 'gates', text: 'Gates Center', value: 'gates center' },
+//   { key: 'hamburg', text: 'Hamburg Hall', value: 'hamburg hall' },
+//   { key: 'hunt', text: 'Hunt Library', value: 'hunt library' },
+//   { key: 'hamerschlag', text: 'Hamerschlag Hall', value: 'hamerschlag hall' },
+//   { key: 'margaret', text: 'Margaret Morrison Hall', value: 'margaret morrison hall' },
+//   { key: 'porter', text: 'Porter Hall', value: 'porter hall' },
+//   { key: 'posner', text: 'Posner Hall', value: 'posner hall' },
+//   { key: 'sorrells', text: 'Sorrells Library', value: 'sorrells library' },
+//   { key: 'tepper', text: 'Tepper Building', value: 'tepper building' },
+//   { key: 'wean', text: 'Wean Hall', value: 'wean hall' },
+// ]
 
 const pickup = [
   { key: 'cohon', text: 'Cohon University Center', value: 'cohon university center' },
@@ -62,15 +62,14 @@ function AddModal() {
           <Modal.Header>Add Item</Modal.Header>
           <Modal.Content>
             {/* Need to stop modal from closing when enter key is pressed */}
-            <Form>
+            <Form onSubmit={(e)=>{e.preventDefault()}}>
               <Form.Input required fluid label='Item Name' placeholder='Item Name'/>
               <Form.Group widths='equal'>
                 <Form.Input required fluid label='Date Found' type='date' placeholder='MM/DD/YYY'/>
                 <Form.Input required fluid label='Time Found' type='time' placeholder='HH:MM'/>
-                {/* Need to manually require location selection?? */}
-                <Form.Select required fluid label='Location Found' options={locations} placeholder='Location'/>    
+                <Form.Input required fluid label='Location Found' placeholder='Location'/>    
               </Form.Group>
-              <Form.TextArea required label='Item Description' placeholder='Item Description' />
+              <Form.Input required label='Item Description' placeholder='Item Description' />
               <Form.Group widths='equal'>
                 <Form.Input fluid type='email' label='Contact Email' placeholder='example@cmu.edu'/>
                 <Form.Select fluid label='Pick-Up Location' options={pickup} placeholder='Pick-Up Location'/>  
@@ -85,7 +84,7 @@ function AddModal() {
               <Form.Group inline id='modal-actions'>
                 <Button onClick={() => dispatch({ type: 'CLOSE_MODAL' })} negative>Cancel</Button>
                 {/* Need to close modal after validation of the form */}
-                <Button type='submit' positive>Add</Button>
+                <Button positive type="submit">Add</Button>
               </Form.Group>
             </Form>
           </Modal.Content>

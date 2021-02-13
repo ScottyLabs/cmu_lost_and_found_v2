c9dd926c7e0cde31c6d65f2006859c04e3632c23
elouiestudent <louie.lizzie@gmail.com>
2021-02-13 15:48:51 -0500

    reworked claim and unclaim buttons

--- client/src/components/ClaimButton.tsx
+++ client/src/components/ClaimButton.tsx
@@ -1,30 +1,48 @@
 import React from "react";
 import axios from "axios";
-import { Button } from "semantic-ui-react";
+import { Icon, Button } from "semantic-ui-react";
 
 // Admin-side claim button that sets backend claim status to claimed
 export default function ClaimButton(props: {
   id: string;
+  disabled: boolean;
   fetchItems: Function
 }) {
+  
+  const handleClick = () => {
+    axios
+      .post(`http://localhost:3080/api/items/updateStatus`, { id: props.id, status: "claimed" })
+      .then(
+        (res) => {
+          console.log("Claimed!");
+          console.log(res);
+          props.fetchItems();
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+
   return (
-    <Button
-      onClick={() => {
-        axios
-          .post(`http://localhost:3080/api/items/updateStatus`, { id: props.id, status: "claimed" })
-          .then(
-            (res) => {
-              console.log("Claimed!");
-              console.log(res);
-              props.fetchItems();
-            },
-            (error) => {
-              console.error(error);
-            }
-          );
-      }}
-    >
-      Claim
-    </Button>
+    props.disabled ?
+      <Button
+        icon
+        circular
+        color="green"
+        size="tiny"
+        disabled
+        onClick={handleClick}
+      >
+        <Icon name="check" inverted size="large"></Icon>
+      </Button> : <Button
+        icon
+        circular
+        color="green"
+        size="tiny"
+        onClick={handleClick}
+      >
+        <Icon name="check" inverted size="large"></Icon>
+      </Button>
   );
 }

1591ea371bf74e0ab7a24e4405c8b2c6e802c4bd
Jaclyn Yang <jackiey321@gmail.com>
2021-02-27 16:48:52 -0500

    Some description div changes

--- client/src/pages/TablePage.tsx
+++ client/src/pages/TablePage.tsx
@@ -9,34 +9,6 @@ import { Item } from "../interface/item";
 import "./TablePage.css";
 
 function TablePage() {
-  // const _items = [
-  //   {
-  //     id:'1',
-  //     dateFound: "11/12/20",
-  //     timeFound: "11:12 am",
-  //     name: "Phone",
-  //     whereFound: "Tepper",
-  //     description: "pink iPhone",
-  //     category: "Phones",
-  //     whereToRetrieve: "gates",
-  //     image: "https://i.pcmag.com/imagery/reviews/03xdTO0Ka4H4KvEgtSPg4c2-12.1569479325.fit_lpad.size_357x209.jpg",
-  //     imagePermission: false,
-  //     status: "available",
-  //   },
-  //   {
-  //     id:'2',
-  //     dateFound: "12/12/20",
-  //     timeFound: "12:12 am",
-  //     name: "Cat",
-  //     whereFound: "Tepper",
-  //     description: "pink iPhone",
-  //     category: "Phones",
-  //     whereToRetrieve: "gates",
-  //     image: "https://i.pcmag.com/imagery/reviews/03xdTO0Ka4H4KvEgtSPg4c2-12.1569479325.fit_lpad.size_357x209.jpg",
-  //     imagePermission: false,
-  //     status: "available",
-  //   },
-  // ];
   const [items, setItems] = useState([]);
 
   //what is from the search
@@ -75,29 +47,18 @@ function TablePage() {
   useEffect(() => {
     fetchItems();
   }, []);
-
-  let description = `
-  Hello, this is the Lost and Found page! `;
-  let description2 = `If you find a lost item, please take it to one of the following locations:`; 
+  
   let locations = ['Gates Hillman 6203', `Lost and Found desk in the University Center`, `Lost and Found desk in Residence on Fifth
   `, `Lost and Found desk in Morewood E-tower`, `Lost and Found desk in Donner`];
-   
-  let description3 = `To retrieve an object, go to the location listed next to the object on the table.  You will be required to identify any lost possessions. 
-  All items must be picked up in person.
-  If you have any inquiries, please send an email to lostfound@cs.cmu.edu. 
-   `;
-  const listItems = locations.map((d) => <div className = "location">{d}</div>);
+
+  const listItems = locations.map((d) => <li className="location">{d}</li>);
+
   return (
     <Grid>
       <Grid.Row>
         <Grid.Column width={16}>
           <main>
-            <Link to="/">
-              <img
-                src="/dog-logo.png"
-                id="logo-mobile"
-                alt="CMU Lost and Found Logo"
-              ></img>
+            <Link to="/"><img src="/dog-logo.png" id="logo-mobile" alt="CMU Lost and Found Logo"></img>
             </Link>
             <div id="settings">
               <Rail attached internal position="left" id="logo-desktop">
@@ -109,10 +70,15 @@ function TablePage() {
             </div>
             <h1 className="title">Carnegie Mellon University</h1>
             <h2 className="subtitle">Lost and Found Website</h2>
-            <div className="description">{description}</div>
-            <div className="description">{description2}</div>
-            {listItems}
-            <div className="description">{description3}</div>
+            <div id="description">
+              <p>If you find a lost item, please take it to one of the following locations:</p>
+              <ul>{listItems}</ul>
+              <p>
+                To retrieve an object, go to the location listed next to the object on the table.  You will be required to identify any lost possessions. 
+                All items must be picked up in person.
+                If you have any inquiries, please send an email to <a href="mailto:lostfound@cs.cmu.edu">lostfound@cs.cmu.edu</a>.
+              </p>
+            </div>
             <div id="center-admin-bar">
               <div id="admin-filter-bar">
                 <FilterBar></FilterBar>

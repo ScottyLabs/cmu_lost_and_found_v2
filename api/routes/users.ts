13ff4f7e49c3139e32bf97f8ce0d92fcb0528b8d
Richard G <rwguo@andrew.cmu.edu>
2021-05-25 21:08:31 -0500

    hid jwt secret

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import User from "../models/User";
+import User, { IUser } from "../models/User";
 import { isUser, isAdmin } from "./auth";
 
 // https://github.com/seanpmaxwell/express-generator-typescript/tree/265df43a2cb23a4389a0361530bb741d1fc88c7b
@@ -58,7 +58,7 @@ router.post('/updatePerm', isAdmin, async (req: Request, res: Response) => {
  */
 router.post('/delete', isAdmin, async (req: Request, res: Response) => {
     let { username } = req.body;
-    User.findOneAndDelete({username: username}, (err, raw) => {
+    User.findOneAndDelete({username: username}, (err: any, raw: IUser) => {
         if (err) {
             console.log(err);
             return res.status(401).send({trace: err, msg: "can't find item in db"});
@@ -66,6 +66,6 @@ router.post('/delete', isAdmin, async (req: Request, res: Response) => {
         return res.status(200).send({msg: raw});
         
     });
-})
+});
 
 export default router;

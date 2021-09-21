66d153f789fa06b7ecf1a75829afe7289d10661d
elouiestudent <louie.lizzie@gmail.com>
2021-09-21 18:53:26 -0400

    made buttons in items outside of building permissions unclickable

--- client/src/components/TableWidget.tsx
+++ client/src/components/TableWidget.tsx
@@ -70,6 +70,9 @@ const TableWidget = (props: {
                 m +
                 " " +
                 (parseInt(h) >= 12 ? "PM" : "AM");
+              let isBuilding = 
+                props.user.permissions.includes(`${item.building}:ADMIN`) ||
+                props.user.permissions.includes(`${item.building}:USER`);
               let isAdmin =
                 props.user.permissions.includes("ALL:ADMIN") ||
                 props.user.permissions.includes(`${item.building}:ADMIN`);
@@ -90,7 +93,7 @@ const TableWidget = (props: {
                       <AvailableSwitch
                         id={item._id}
                         isAvailable={item.status === "available"}
-                        disabled={false}
+                        disabled={!isBuilding}
                         fetchItems={props.fetchItems}
                       ></AvailableSwitch>
                     </Table.Cell>
@@ -102,7 +105,7 @@ const TableWidget = (props: {
                         user={props.user}
                         item={item}
                         id={item._id}
-                        disabled={false}
+                        disabled={!isBuilding}
                       ></EditButton>
                     </Table.Cell>
                   ) : null}

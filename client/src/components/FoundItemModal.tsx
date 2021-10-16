be3e07caacc5af8324366df37f25112814f6df71
Michelle Li <mli6@andrew.cmu.edu>
2021-10-16 15:08:18 -0400

    Add section to give feedback

--- client/src/components/FoundItemModal.tsx
+++ client/src/components/FoundItemModal.tsx
@@ -47,7 +47,14 @@ export const foundItemMessage = (
   </>
 );
 
-
+export const feedbackForm = (
+  <>
+    <p>
+      To leave some feedback, please fill out this {" "}
+      <a href="https://forms.gle/QDnNyjdzUBnFUkno8" target="_blank">form</a>. Thanks!
+    </p>
+  </>
+);
 
 export const lostItemMessage = (
   <>

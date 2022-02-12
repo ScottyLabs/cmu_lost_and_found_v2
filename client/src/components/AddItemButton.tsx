11a0cc7598bb374dc2fa37b0bf7a627bc6dbe762
Brian-T-Lee <brianle2@andrew.cmu.edu>
2022-02-12 15:21:10 -0500

    added optional fields for sending email

--- client/src/components/AddItemButton.tsx
+++ client/src/components/AddItemButton.tsx
@@ -9,6 +9,7 @@ import { BuildingType } from "../enums/locationTypes";
 import { User } from "../interface/user";
 import emailbody from "../templates/html/emailbody";
 import { PermissionType } from "../enums/permissionType";
+import { TemplateType } from "../enums/templateTypes";
 
 function exampleReducer(dispatchState: any, action: any) {
   switch (action.type) {
@@ -66,6 +67,23 @@ const buildings = Object.keys(BuildingType)
     value: key,
   }));
 
+const templates = Object.keys(TemplateType)
+.filter((value) => value !== "ALL")
+.map((key) => ({
+  key,
+  text: key,
+  value: key,
+}));
+
+const emailValidatorRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Source: https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+
+const isValidEmail = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(emailValidatorRegex);
+  };
+
 function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
   const [dispatchState, dispatch] = React.useReducer(exampleReducer, {
     closeOnEscape: false,
@@ -89,6 +107,8 @@ function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
     imagePermission: false,
     status: "available",
     identification: "",
+    email: "",
+    templateType: "",
     notes: "",
   });
 
@@ -201,6 +221,8 @@ function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
       imagePermission,
       status,
       identification,
+      email,
+      templateType,
       notes,
     } = state;
 
@@ -312,6 +334,8 @@ function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
             status: status,
             approved: props.isAdmin,
             identification: identification,
+            email: email,
+            templateType: templateType,
             notes: notes,
           })
           .then(
@@ -346,6 +370,8 @@ function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
           imagePermission: false,
           status: "available",
           identification: "",
+          email: "",
+          templateType: "",
           notes: "",
         });
         return res;
@@ -482,13 +508,36 @@ function AddItemButton(props: { fetchItems: Function; isAdmin: boolean }) {
                 value={state.imagePath}
                 onChange={handleFileChange}
               />
+              { state.identifiable ? 
               <Form.Input
                 label="Identification"
                 placeholder="AndrewID or driver's license number"
                 name="identification"
                 value={state.identification}
                 onChange={handleChange}
-              />
+              /> 
+              : null }
+              { state.identifiable ? 
+              <Form.Input
+                label="Email"
+                placeholder="Ex. bovick@andrew.cmu.edu"
+                name="email"
+                value={state.email}
+                onChange={handleChange}
+              /> 
+              : null }
+              { isValidEmail(state.email) ? 
+              <Form.Select
+                fluid
+                required
+                label="Template Type"
+                options={templates}
+                placeholder="CMU ID or item with PID"
+                name="templateType"
+                value={state.templateType}
+                onChange={handleChange}
+              /> 
+              : null }
               <Form.TextArea
                 label="Notes"
                 name="notes"

f9914e7798731fe4dffe6788979e2757fd1b93a1
Nandini Neralagi <nandinineralagi@nandinis-mbp-3.wifi.local.cmu.edu>
2022-02-26 14:37:11 -0500

    added location based searching for issue #85

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -79,13 +79,16 @@ function Admin() {
 
   // modify items
   const updateInput = async (input: string) => {
+    let inputName = input.toLowerCase();
     const filtered = itemListDefault.filter((item: Item) => {
-      return item.name.toLowerCase().includes(input.toLowerCase());
+      return (
+        item.whereFound.toLowerCase().includes(inputName));
     });
     setInput(input);
     setItemList(filtered);
     setPage(1);
   };
+  
 
   // sort items
   const sortItems = async (column: string, direction: string) => {

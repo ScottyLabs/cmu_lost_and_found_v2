d621942cd11c43a026a90811056c00ed0326421a
Michelle Li <mli6@andrew.cmu.edu>
2022-03-19 16:12:31 -0400

    attempt

--- client/src/components/DownloadDataButton.tsx
+++ client/src/components/DownloadDataButton.tsx
@@ -11,6 +11,7 @@ import emailbody from "../templates/html/emailbody";
 import { PermissionType } from "../enums/permissionType";
 import { TemplateType } from "../enums/templateTypes";
 import { UseTemplate } from "../templates/emailTemplates";
+import MongoClient from "mongodb";
 
 function exampleReducer(dispatchState: any, action: any) {
   switch (action.type) {
@@ -30,6 +31,34 @@ function exampleReducer(dispatchState: any, action: any) {
 function DownloadDataButton(props: { fetchItems: Function; isAdmin: boolean }) {
     
   const printy = () => {
+    // const mongodb = require("mongodb").MongoClient;
+    // const fastcsv = require("fast-csv");
+    // const fs = require("fs");
+    // const ws = fs.createWriteStream("bezkoder_mongodb_fastcsv.csv");
+    // // let url = "mongodb://username:password@localhost:27017/";
+    // let url = process.env.MONGO_URI || "mongodb://localhost:27017";
+    // mongodb.connect(
+    //   url,
+    //   { useNewUrlParser: true, useUnifiedTopology: true },
+    //   (err, client) => {
+    //     if (err) throw err;
+    //     client
+    //       .db("zkoder_db")
+    //       .collection("category")
+    //       .find({})
+    //       .toArray((err, data) => {
+    //         if (err) throw err;
+    //         console.log(data);
+    //         fastcsv
+    //           .write(data, { headers: true })
+    //           .on("finish", function() {
+    //             console.log("Write to bezkoder_mongodb_fastcsv.csv successfully!");
+    //           })
+    //           .pipe(ws);
+    //         client.close();
+    //       });
+    //   }
+    // );
     console.log("bruh");
   };
 

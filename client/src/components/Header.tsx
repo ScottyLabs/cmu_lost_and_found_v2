f294078b52537a74285e48c0e08f690b884c7ab7
Jaclyn Yang <jackiey321@gmail.com>
2022-04-16 21:54:09 -0400

    created header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DropdownMenu from "../components/DropdownMenu";
+import LogoutButton from "../components/LogoutButton";
+import "./Header.css";
+
+const Footer = (props: any) => {
+  return (
+    <header>
+      <div id="site-identity">
+        <Link to="/" >
+          <img src="/dog-logo.png" id="logo-mobile" alt="CMU Lost and Found Logo"></img>
+          <img src="/dog-logo.png" id="logo-desktop" alt="CMU Lost and Found Logo"></img>
+        </Link>
+        <div id="page-titles">
+          <h1 className="title">Carnegie Mellon University</h1>
+          <h2 className="subtitle">Lost and Found Website</h2>
+        </div>
+      </div>
+      <div id="navigation">
+        <LogoutButton />
+        <DropdownMenu page={"/"} isAdmin={props.isAdmin} isAllAdmin={props.isAllAdmin}/>
+      </div>
+    </header>
+  );
+};
+
+export default Footer;

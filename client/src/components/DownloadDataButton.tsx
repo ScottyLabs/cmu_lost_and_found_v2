24839439a48d8fc2de3fd1e26e22cecc7b383771
Brian-T-Lee <brianle2@andrew.cmu.edu>
2022-06-04 19:17:27 -0700

    CSV download functionality (manual) added

--- client/src/components/DownloadDataButton.tsx
+++ client/src/components/DownloadDataButton.tsx
@@ -36,28 +36,82 @@ function DownloadDataButton(props: { fetchItems: Function; items: Item[] }) {
     /**
      * This section is not compatible with static site generation
      */
-    // const ObjectsToCsv = require('objects-to-csv');
-    // (async () => {
-    //   const newArr = props.items.map(({
-    //     _id,
-    //     id,
-    //     imagePermission,
-    //     approved,
-    //     publicDisplay,
-    //     whereToRetrieve,
-    //     ...rest}) => {
-    //     return rest;
-    //   });
-    //   const csv = new ObjectsToCsv(newArr);
-    //   const csvItems = await csv.toString();
-    //   element.href = 'data:attachment/csv,' +  encodeURIComponent(csvItems);
-    //   element.target = '_blank';
-    //   element.download = 'lostAndFoundData.csv';
-    //   element.style.display = 'none';
-    //   document.body.appendChild(element);
-    //   element.click();
-    //   document.body.removeChild(element);
-    // })();
+
+    (async () => {
+      const newArr = props.items.map(({
+        _id,
+        id,
+        imagePermission,
+        approved,
+        publicDisplay,
+        whereToRetrieve,
+        ...rest}) => {
+        return rest;
+      });
+
+      // Convert object to json
+      const objectToCsv = (data : Object[]) => {
+        const csvRows = [];
+        const headers = Object.keys(data[0]);
+        csvRows.push(headers.join(','));
+  
+        // Grabs the object key list so csv entries are in a fixed order
+        let trueValues = [] as string[];
+        for (const row of data) {
+          const keyList = Object.keys(row);
+          if (trueValues.length < keyList.length) 
+            trueValues = keyList;
+        }
+
+        // Loop to get value of each objects key
+        for (const row of data) {
+          // "array" that allows for the empty entries to exist
+          const values = trueValues.map(tag => row[tag as keyof typeof row]);
+
+          /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+           Merges any subarrays into a string. 
+           Ex. [[a,b,c], x2, x3, [a,b,d]]
+           turns into
+            [a; b; c, x2, x3, a; b; d]
+           because join(",") would otherwise make the string 
+            a,b,c,x2,x3,a,b,d 
+           which gets screwy in the CSV. 
+           * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+          const valueArray = Array.from(values);
+          const valueSingled = [];
+          for (const val of valueArray) {
+            if (Array.isArray(val)) {
+              valueSingled.push(val.join(", "))
+            } else {
+              valueSingled.push(val)
+            }
+          }
+
+          // Puts quotes around strings to allow for commas in CSV format
+          const valueCleaned = [];
+          for (const val of valueSingled) {
+            if (typeof val == 'string' || val instanceof String) {
+              valueCleaned.push("\"" + val + "\"")
+            } else {
+              valueCleaned.push(val)
+            }
+          }
+
+          // Removes newline cuz it can break CSVs (quotes should fix it but just to be safe)
+          const strippedValues = valueCleaned.join(',').replace(/(\r\n|\n|\r)/gm, "")
+          csvRows.push(strippedValues);
+        }
+        return csvRows.join('\n');
+      };
+
+      const objItem = objectToCsv(newArr);
+      const element = document.createElement("a");
+      const file = new Blob([objItem],{type:'application/json'});
+      element.href = URL.createObjectURL(file);
+      element.download = "lostAndFoundData.csv";
+      document.body.appendChild(element);
+      element.click();
+    })();
   }  
   
   return (

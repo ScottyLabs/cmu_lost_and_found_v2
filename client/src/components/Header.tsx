adb10dd6e9e1e317456ba3cf61dfd5c8272ed7d6
Jaclyn Yang <jackiey321@gmail.com>
2022-10-08 15:33:46 -0400

    fixed linter errors

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import DropdownMenu from "../components/DropdownMenu";
 import LogoutButton from "../components/LogoutButton";
+
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = (props: {
@@ -10,15 +11,25 @@ const Header = (props: {
   isAllAdmin: boolean;
 }) => {
   const pageTitle = () => {
-    const withoutSlash = props.page.slice(1)
-    return withoutSlash.length == 0 ? "" : "- " + withoutSlash.charAt(0).toUpperCase() + withoutSlash.slice(1)
-  }
+    const withoutSlash = props.page.slice(1);
+    return withoutSlash.length == 0
+      ? ""
+      : "- " + withoutSlash.charAt(0).toUpperCase() + withoutSlash.slice(1);
+  };
   return (
     <header>
       <div id="site-identity">
-        <Link to="/" >
-          <img src="/dog-logo.png" id="logo-mobile" alt="CMU Lost and Found Logo"></img>
-          <img src="/dog-logo.png" id="logo-desktop" alt="CMU Lost and Found Logo"></img>
+        <Link to="/">
+          <img
+            src="/dog-logo.png"
+            id="logo-mobile"
+            alt="CMU Lost and Found Logo"
+          ></img>
+          <img
+            src="/dog-logo.png"
+            id="logo-desktop"
+            alt="CMU Lost and Found Logo"
+          ></img>
         </Link>
         <div id="page-titles">
           <h1 className="title">Carnegie Mellon University</h1>
@@ -26,7 +37,11 @@ const Header = (props: {
         </div>
       </div>
       <div id="navigation">
-        <DropdownMenu page={props.page} isAdmin={props.isAdmin} isAllAdmin={props.isAllAdmin}/>
+        <DropdownMenu
+          page={props.page}
+          isAdmin={props.isAdmin}
+          isAllAdmin={props.isAllAdmin}
+        />
         <LogoutButton />
       </div>
     </header>

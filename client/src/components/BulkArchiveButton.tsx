f88dfd542eab7f0706ecb22df4d9f4e10c95012f
elouiestudent <louie.lizzie@gmail.com>
2022-10-27 18:03:17 -0400

    Add modal for bulk archive button

--- client/src/components/BulkArchiveButton.tsx
+++ client/src/components/BulkArchiveButton.tsx
@@ -1,46 +1,101 @@
 import axios from "axios";
 import * as React from "react";
 import { useHistory } from "react-router";
-import { Button } from "semantic-ui-react";
+import { Button, Modal } from "semantic-ui-react";
 
-export default function BulkArchiveButton(props: {
-  // TODO: #128 Replace Function type with appropriate type
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  fetchItems: Function;
-}) {
+function exampleReducer(dispatchState: any, action: any) {
+  switch (action.type) {
+    case "CONFIG_CLOSE_ON_DIMMER_CLICK":
+      return { ...dispatchState, closeOnDimmerClick: action.value };
+    case "CONFIG_CLOSE_ON_ESCAPE":
+      return { ...dispatchState, closeOnEscape: action.value };
+    case "OPEN_MODAL":
+      return { ...dispatchState, open: true };
+    case "CLOSE_MODAL":
+      return { ...dispatchState, open: false };
+    default:
+      throw new Error();
+  }
+}
+
+export default function BulkArchiveButton(props: { fetchItems: () => void }) {
+  const [dispatchState, dispatch] = React.useReducer(exampleReducer, {
+    closeOnEscape: false,
+    closeOnDimmerClick: false,
+    open: false,
+    dimmer: undefined,
+  });
+  const { open, closeOnEscape, closeOnDimmerClick } = dispatchState;
+
   const history = useHistory();
+
   return (
-    <Button
-      color="red"
-      style={{
-        height: "47px",
-        width: "110px",
-        margin: "0px",
-        marginRight: "10px",
-      }}
-      onClick={() => {
-        axios
-          .post("/api/items/archiveByDays", {
-            token: localStorage.getItem("lnf_token"),
-            days: 90,
-          })
-          .then(
-            (res) => {
-              console.log("Bulk archived!");
-              console.log(res);
-              props.fetchItems();
-            },
-            (error) => {
-              console.log(error);
-              if (error?.response?.status === 401) {
-                window.localStorage.removeItem("lnf_token");
-                history.push("/login");
-              }
-            }
-          );
-      }}
+    <Modal
+      closeOnEscape={closeOnEscape}
+      closeOnDimmerClick={closeOnDimmerClick}
+      open={open}
+      onOpen={() => dispatch({ type: "OPEN_MODAL" })}
+      onClose={() => dispatch({ type: "CLOSE_MODAL" })}
+      trigger={
+        <Button
+          color="red"
+          style={{
+            height: "47px",
+            width: "110px",
+            margin: "0px",
+            marginRight: "10px",
+          }}
+        >
+          Bulk Archive
+        </Button>
+      }
     >
-      Bulk Archive
-    </Button>
+      <Modal.Header>Bulk Archive</Modal.Header>
+      <Modal.Content
+        style={{
+          margin: "auto",
+          maxWidth: "100%",
+          padding: "20px 20px 50px 20px",
+          fontSize: "18px",
+        }}
+      >
+        <p>Are you sure you wish to archive all items older than 90 days?</p>
+        <Button
+          onClick={() => {
+            axios
+              .post("/api/items/archiveByDays", {
+                token: localStorage.getItem("lnf_token"),
+                days: 90,
+              })
+              .then(
+                (res) => {
+                  console.log("Bulk archived!");
+                  console.log(res);
+                  props.fetchItems();
+                },
+                (error) => {
+                  console.log(error);
+                  if (error?.response?.status === 401) {
+                    window.localStorage.removeItem("lnf_token");
+                    history.push("/login");
+                  }
+                }
+              );
+            dispatch({ type: "CLOSE_MODAL" });
+          }}
+          positive
+          floated="right"
+        >
+          Yes
+        </Button>
+        <Button
+          onClick={() => dispatch({ type: "CLOSE_MODAL" })}
+          negative
+          floated="right"
+        >
+          Cancel
+        </Button>
+      </Modal.Content>
+    </Modal>
   );
 }

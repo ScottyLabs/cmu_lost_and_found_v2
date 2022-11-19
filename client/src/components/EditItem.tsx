d38b16f489e82400e4750dbac18937abacdb2033
elouiestudent <louie.lizzie@gmail.com>
2022-11-19 15:08:03 -0500

    Make archiving only available to admins

--- client/src/components/EditItem.tsx
+++ client/src/components/EditItem.tsx
@@ -275,6 +275,10 @@ function EditItem(props: {
     );
   };
 
+  const isAdmin = props.user?.permissions.some((value) =>
+    value.includes(PermissionType.ADMIN)
+  );
+
   return (
     <Grid columns={1}>
       <Grid.Column>
@@ -438,16 +442,18 @@ function EditItem(props: {
                       width: "10px",
                     }}
                   />
-                  <ArchiveButton
-                    id={props.id}
-                    fetchItems={props.fetchItems}
-                    disabled={
-                      props.item.approved &&
-                      !props.user.permissions.some((value) =>
-                        value.includes(PermissionType.ADMIN)
-                      )
-                    }
-                  />
+                  {isAdmin ? (
+                    <ArchiveButton
+                      id={props.id}
+                      fetchItems={props.fetchItems}
+                      disabled={
+                        props.item.approved &&
+                        !props.user.permissions.some((value) =>
+                          value.includes(PermissionType.ADMIN)
+                        )
+                      }
+                    />
+                  ) : null}
                 </div>
                 <div
                   style={{

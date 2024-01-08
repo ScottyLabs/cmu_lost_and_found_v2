f89e0b7e8507dfc3526be50b4db6c0d905f5e36f
Gabriel <adrastopoulos@gmail.com>
2024-01-07 17:29:58 -0800

    chore: remove web vitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,4 @@
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import * as React from "react";
 import { render } from "react-dom";
@@ -16,8 +15,3 @@ render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
